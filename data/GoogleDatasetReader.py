6404fff70d7ae2fae13625409d7fa2430d5b6a51
Ferran Espuña <fespunbe7@alumnes.ub.edu>
2022-11-26 12:11:03 +0100

    iterator

--- data/GoogleDatasetReader.py
+++ data/GoogleDatasetReader.py
@@ -29,7 +29,7 @@ def _load_train_data(dataset_location):
 
 def _load_data(dataset_location):
     path_to_shards = glob.glob(os.path.join(dataset_location, 'shard_*.tfrecord'))
-    dataset = tf.data.TFRecordDataset([path_to_shards[0]])
+    dataset = tf.python_io.tf_record_iterator(path_to_shards)[:1000]
     print(len(dataset))
     return dataset.map(_deserialize_example)
 

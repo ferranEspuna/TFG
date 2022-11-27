3a4e484fd1f568580778ff1a541b87cb302657d2
Ferran Espuña <fespunbe7@alumnes.ub.edu>
2022-11-27 12:14:38 +0100

    aun mas minimal

--- minimalExample.py
+++ minimalExample.py
@@ -3,24 +3,7 @@ import os
 import tensorflow as tf
 
 
-def _deserialize_example(serialized_example):
-    record = tf.io.parse_single_example(
-        serialized_example,
-        features={
-            'inputs': tf.io.FixedLenFeature([], tf.string),
-            'output': tf.io.FixedLenFeature([], tf.string)
-        })
-    inputs = tf.io.parse_tensor(record['inputs'], out_type=tf.float32)
-    output = tf.io.parse_tensor(record['output'], out_type=tf.int32)
-    return inputs, output
-
-
-print(1)
 loc = "./data/Google/public_data/input_data/task1_v4/dataset_1/train"
-print(2)
 abs_loc = os.path.abspath(loc)
-print(3)
 path_to_shards = glob.glob(os.path.join(abs_loc, 'shard_*.tfrecord'))
-print(4)
-dataset = tf.data.TFRecordDataset(path_to_shards)
-print(5)
+dataset = tf.data.TFRecordDataset(path_to_shards[0])

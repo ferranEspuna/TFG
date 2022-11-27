807093ec2d30092b74d1117264a12ea7e8eedca0
Ferran Espuña <fespunbe7@alumnes.ub.edu>
2022-11-27 12:39:09 +0100

    wtff

--- minimalExample.py
+++ minimalExample.py
@@ -2,6 +2,11 @@ import glob
 import os
 import tensorflow as tf
 
+gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.333)
+print('options')
+sess = tf.compat.v1.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+print('session')
+
 loc = "./data/Google/public_data/input_data/task1_v4/dataset_1/train"
 abs_loc = os.path.abspath(loc)
 path_to_shards = glob.glob(os.path.join(abs_loc, 'shard_*.tfrecord'))

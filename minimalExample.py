76691b48f1f6b27a1a84cd23b710e9ca3fb82d07
Ferran Espuña <fespunbe7@alumnes.ub.edu>
2022-11-27 13:06:49 +0100

    mal

--- minimalExample.py
+++ minimalExample.py
@@ -2,7 +2,7 @@ import glob
 import os
 import tensorflow as tf
 
-loc = "./data/Google/public_data/input_data/task1_v4/dataset_1/train"
+loc = "./data/Google/public_data/input_data/task1_v4/dataset_1/trainihgiuh"
 abs_loc = os.path.abspath(loc)
 path_to_shards = glob.glob(os.path.join(abs_loc, 'shard_*.tfrecord'))
 dataset = tf.data.TFRecordDataset(path_to_shards)

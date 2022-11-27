7770f5f18f049c35fc54eb56ae32b3ca0bacadb5
Ferran Espuña <fespunbe7@alumnes.ub.edu>
2022-11-27 13:03:57 +0100

    minimal

--- minimalExample.py
+++ minimalExample.py
@@ -1,16 +1,8 @@
-#import glob
-#import os
-#import tensorflow as tf
-import numpy as np
+import glob
+import os
+import tensorflow as tf
 
-print('not allocated')
-ar = np.zeros((1024, 1024, 1024, 50), dtype=int)
-print('allocated')
-
-"""
 loc = "./data/Google/public_data/input_data/task1_v4/dataset_1/train"
 abs_loc = os.path.abspath(loc)
 path_to_shards = glob.glob(os.path.join(abs_loc, 'shard_*.tfrecord'))
 dataset = tf.data.TFRecordDataset(path_to_shards)
-
-"""
